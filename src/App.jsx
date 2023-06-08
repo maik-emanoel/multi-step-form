d9c3057d42acd6cc8ab858b1a73e8c60151b3703
Maik Emanoel <maikemanoel11@gmail.com>
2023-06-08 19:57:29 -0300

    add hr tag

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import Header from "./components/Header"
 export default function App() {
     return (
        <div className="max-w-[592px] w-full border-[1px] border-gray-100 rounded-lg shadow-[0rem_0.4rem_1.6rem_#EAE2FD] p-8">
-        <Header />
+            <Header />
+            <hr className="my-8 bg-gray-100 opacity-40" />
        </div>
     )
 }
