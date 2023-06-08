62722761ded67ff0e2bf04515150b9d0a9af4c91
Maik Emanoel <maikemanoel11@gmail.com>
2023-06-08 19:57:00 -0300

    add content to header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,26 +1,32 @@
-import chevronRight from '../../assets/chevronRight.svg'
+import chevronRight from "../../assets/chevronRight.svg";
 
 export default function Header() {
   return (
-    <div className='flex justify-between items-center h-11 font-bold'>
-        <div className='flex gap-2 items-center'>
-            <span className='w-7 h-7 flex items-center justify-center rounded-full'>1</span>
-            <p className='text-gray-400'>Contato</p>
-        </div>
+    <div className="flex justify-between items-center h-11 font-bold">
+      <div className="flex gap-2 items-center px-2">
+        <span className="w-7 h-7 flex items-center justify-center rounded-full text-sm additionalFont">
+          1
+        </span>
+        <p className="text-gray-400">Contato</p>
+      </div>
 
-        <img src={chevronRight} alt="Seta para a direita" className='w-5 h-5' />
+      <img src={chevronRight} alt="Seta para a direita" className="w-5 h-5" />
 
-        <div className='flex gap-2 items-center'>
-            <span className='w-7 h-7 flex items-center justify-center rounded-full'>2</span>
-            <p className='text-gray-400'>Empresa</p>
-        </div>
+      <div className="flex gap-2 items-center px-2">
+        <span className="w-7 h-7 flex items-center justify-center rounded-full text-sm additionalFont">
+          2
+        </span>
+        <p className="text-gray-400">Empresa</p>
+      </div>
 
-        <img src={chevronRight} alt="Seta para a direita" className='w-5 h-5' />
+      <img src={chevronRight} alt="Seta para a direita" className="w-5 h-5" />
 
-        <div className='flex gap-2 items-center'>
-            <span className='w-7 h-7 flex items-center justify-center rounded-full'>3</span>
-            <p className='text-gray-400'>Projeto</p>
-        </div>
+      <div className="flex gap-2 items-center px-2">
+        <span className="w-7 h-7 flex items-center justify-center rounded-full text-sm additionalFont">
+          3
+        </span>
+        <p className="text-gray-400">Projeto</p>
+      </div>
     </div>
   );
 }
