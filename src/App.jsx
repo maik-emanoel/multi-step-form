bb7a84da3cff09d1375cf1250ca961cf3286a0a0
Maik Emanoel <maikemanoel11@gmail.com>
2023-06-09 14:16:56 -0300

    add main & bottom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import Header from "./components/Header"
+import Header from "./components/Header";
+import Button from "./components/Button";
+import BackButton from "./components/BackButton";
+
+import Step1 from "./components/Step1";
 
 export default function App() {
+  const showBackButton = false;
+
     return (
-       <div className="max-w-[592px] w-full border-[1px] border-gray-100 rounded-lg shadow-[0rem_0.4rem_1.6rem_#EAE2FD] p-8">
+        <div className="max-w-[592px] w-full border-[1px] border-gray-100 rounded-lg shadow-[0rem_0.4rem_1.6rem_#EAE2FD] p-8">
             <Header />
             <hr className="my-8 bg-gray-100 opacity-40" />
-       </div>
-    )
+
+            <main className="flex flex-col gap-4 mb-8">
+                <Step1 />
+            </main>
+
+            <div className={`flex ${showBackButton ? "justify-between" : "justify-end"}`}>
+                {showBackButton && <BackButton name="Voltar" />}
+                <Button name="Continuar" />
+            </div>
+        </div>
+    );
 }
