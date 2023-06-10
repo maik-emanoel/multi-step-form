82338764c1f101b599017874e902ef9ab26d3ed4
Maik Emanoel <maikemanoel11@gmail.com>
2023-06-10 16:09:55 -0300

    add function to handle prevStep & nextStep

--- src/components/BackButton/index.jsx
+++ src/components/BackButton/index.jsx
@@ -1,7 +1,10 @@
 // eslint-disable-next-line react/prop-types
-export default function BackButton({ name }) {
+export default function BackButton( { name, handlePreviousStep } ) {
     return (
-        <button className="max-w-[158px] w-full h-14 rounded-md uppercase text-purpleMid font-bold border border-purpleMid">
+        <button 
+            className="max-w-[158px] w-full h-14 rounded-md uppercase text-purpleMid font-bold border border-purpleMid"
+            onClick={handlePreviousStep}
+        >
             {name}
         </button>
     )
--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,10 @@
 // eslint-disable-next-line react/prop-types
-export default function Button( { name } ) {
+export default function Button( { name, handleNextStep } ) {
     return (
-        <button className="max-w-[158px] w-full h-14 rounded-md bg-purpleMid uppercase text-white font-bold hover:bg-purplelight">
+        <button 
+            className="max-w-[158px] w-full h-14 rounded-md bg-purpleMid uppercase text-white font-bold hover:bg-purplelight"
+            onClick={handleNextStep}
+        >
             {name}
         </button>
     )
