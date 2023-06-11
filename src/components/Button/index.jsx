6586406426d260f50ef9bab3acf8768a053d56fd
Maik Emanoel <maikemanoel11@gmail.com>
2023-06-11 15:27:17 -0300

    set disabled

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,9 +1,10 @@
 // eslint-disable-next-line react/prop-types
-export default function Button( { name, handleNextStep } ) {
+export default function Button( { name, handleNextStep, disabled } ) {
     return (
         <button 
-            className="max-w-[158px] w-full h-14 rounded-md bg-purpleMid uppercase text-white font-bold hover:bg-purplelight"
+            className="py-4 px-8 rounded-md bg-purpleMid uppercase text-white font-bold hover:bg-purplelight disabled:cursor-not-allowed disabled:opacity-80 disabled:bg-purpleMid"
             onClick={handleNextStep}
+            disabled={disabled}
         >
             {name}
         </button>
